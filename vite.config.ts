8dfbcdb7fbe22eae7edca8886abed0f48329c6c0
MiloradGlog <miloradst@hotmail.com>
2025-10-07 14:42:58 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: 3000,
+      allowedHosts: env.VITE_ADMIN_WEB_URL ? [new URL(env.VITE_ADMIN_WEB_URL).hostname] : undefined,
       watch: {
         usePolling: true,
       },
